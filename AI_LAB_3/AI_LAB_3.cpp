fc86eda6ce2d477b0a0ea19afae52f2c54ae3cb5
Данила Иванов <danila2003ivanov03@gmail.com>
2023-03-11 23:04:23 +0300

    test

--- AI_LAB_3/AI_LAB_3.cpp
+++ AI_LAB_3/AI_LAB_3.cpp
@@ -7,9 +7,23 @@ using namespace std;
 const int number = 10; //кол - во чисел
 const int column = 15; //кол - во столбцов
 
-int MAIN_FUNC(const int num[], int main_selection[][column+1]) {
+int main_selection[number][column + 1] = {
+		{0, 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15},
+		{1, 2,3,4,5,6,7,8,9,10,11,12,13,14,15,1},
+		{2, 3,4,5,6,7,8,9,10,11,12,13,14,15,1,2},
+		{3, 4,5,6,7,8,9,10,11,12,13,14,15,1,2,3},
+		{4, 5,6,7,8,9,10,11,12,13,14,15,1,2,3,4},
+		{5, 6,7,8,9,10,11,12,13,14,15,1,2,3,4,5},
+		{6, 7,8,9,10,11,12,13,14,15,1,2,3,4,5,6},
+		{7, 8,9,10,11,12,13,14,15,1,2,3,4,5,6,7},
+		{8, 9,10,11,12,13,14,15,1,2,3,4,5,6,7,8},
+		{9, 10,11,12,13,14,15,1,2,3,4,5,6,7,8,9},
+};
 
-	start:
+
+int MAIN_FUNC(int counter_check[], const int num[]) {
+
+start:
 	bool flag = false;
 
 	vector <int> weights = {}; //вектор полученных весов
@@ -26,7 +40,7 @@ int MAIN_FUNC(const int num[], int main_selection[][column+1]) {
 	int temp_max = *max_element(weights.begin(), weights.end());
 	for (int i = 0; i < weights.size(); i++) {
 		if (weights[i] >= temp_max) { number_weight.push_back(i); } //если пришли одиновые макс числа, то записываем их номер тоже
-	}	
+	}
 
 	if (num[0] == number_weight[0] and number_weight.size() == 1) { flag = true; } //веса побранны
 	else {
@@ -49,9 +63,9 @@ int MAIN_FUNC(const int num[], int main_selection[][column+1]) {
 		}
 		//cout << endl;
 	}
-
-	if(flag != true) { goto start; }
-	return main_selection[number][column + 1];
+	cout << "Check: " << counter_check[0] << endl;
+	if (flag != true) { counter_check[0] = 0; goto start; }
+	return counter_check[0] += 1;
 }
 
 int main() {
@@ -59,18 +73,14 @@ int main() {
 
 	setlocale(LC_ALL, "Russian");
 
-	int main_selection[number][column + 1] = {
-		{0, 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15},
-		{1, 2,3,4,5,6,7,8,9,10,11,12,13,14,15,1},
-		{2, 3,4,5,6,7,8,9,10,11,12,13,14,15,1,2},
-		{3, 4,5,6,7,8,9,10,11,12,13,14,15,1,2,3},
-		{4, 5,6,7,8,9,10,11,12,13,14,15,1,2,3,4},
-		{5, 6,7,8,9,10,11,12,13,14,15,1,2,3,4,5},
-		{6, 7,8,9,10,11,12,13,14,15,1,2,3,4,5,6},
-		{7, 8,9,10,11,12,13,14,15,1,2,3,4,5,6,7},
-		{8, 9,10,11,12,13,14,15,1,2,3,4,5,6,7,8},
-		{9, 10,11,12,13,14,15,1,2,3,4,5,6,7,8,9},
-	};
+	cout << "Начальная матрица весов" << endl;
+	for (int i = 0; i < number; i++) {
+		for (int j = 1; j < column + 1; j++) {
+			cout << main_selection[i][j] << " ";
+		}cout << endl;
+	}
+
+	int counter_check[1] = { 0 };
 
 	//Цифры в двоичном предствалении
 	const int number_0[column + 1] = {0, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1 }; //0
@@ -84,19 +94,22 @@ int main() {
 	const int number_8[column + 1] = {8, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1 }; //8
 	const int number_9[column + 1] = {9, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1, 0, 1, 0, 0 }; //9
 
-	for (int i = 0; i < 20; i++) {
-		MAIN_FUNC(number_0, main_selection);
-		MAIN_FUNC(number_1, main_selection);
-		MAIN_FUNC(number_2, main_selection);
-		MAIN_FUNC(number_3, main_selection);
-		MAIN_FUNC(number_4, main_selection);
-		MAIN_FUNC(number_5, main_selection);
-		MAIN_FUNC(number_6, main_selection);
-		MAIN_FUNC(number_7, main_selection);
-		MAIN_FUNC(number_8, main_selection);
-		MAIN_FUNC(number_9, main_selection);
+	while (counter_check[0] < 10) {
+				
+		MAIN_FUNC(counter_check, number_0);
+		MAIN_FUNC(counter_check, number_1);
+		MAIN_FUNC(counter_check, number_2);
+		MAIN_FUNC(counter_check, number_3);
+		MAIN_FUNC(counter_check, number_4);
+		MAIN_FUNC(counter_check, number_6);
+		MAIN_FUNC(counter_check, number_7);
+		MAIN_FUNC(counter_check, number_8);
+		MAIN_FUNC(counter_check, number_9);
+		//cout << "Счетчик неизменений: " << counter_check[0] << endl;
+
 	}
 
+	cout << "Персептрон обучился" << endl;
 	for (int i = 0; i < number; i++) {
 		for (int j = 1; j < column + 1; j++) {
 			cout << main_selection[i][j] << " ";
